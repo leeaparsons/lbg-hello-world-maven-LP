73274294831b5d8bf68c7a369a1d89cea9dcd486
LeeP <lee.aparsons@hotmail.com>
2024-11-04 14:49:04 +0000

    tetsing vsc commit

--- src/main/java/com/qa/App.java
+++ src/main/java/com/qa/App.java
@@ -13,6 +13,7 @@ public class App
         System.out.println(sayHelloToSomeone("Bob"));
         System.out.println(sayHelloToSomeone("Charlie"));
         System.out.println(sayHelloToSomeone("Zena"));
+        System.out.println("LEE IS HERE OH MY GOD")
         System.out.println(sayGoodbye());
     }
 
